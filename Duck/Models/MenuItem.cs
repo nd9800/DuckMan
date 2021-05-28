6f6ab8efec27f3b9445a308b072ddcd016a4b5a7
NamD <64962991+nd9800@users.noreply.github.com>
2021-05-28 20:37:52 +0700

    285

---
+++ Duck/Models/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duck.Models
+{
+    class MenuItem
+    {
+        public string Icon { set; get; }
+        public string Title { set; get; }
+
+        public MenuItem(string icon, string title)
+        {
+            Icon = icon;
+            Title = title;
+        }
+    }
+}
--- Duck/Pages/Home.xaml.cs
+++ Duck/Pages/Home.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Duck.Models;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +26,29 @@ namespace Duck.Pages
         public Home()
         {
             this.InitializeComponent();
+        }
+
+        private void Menu_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            MenuItem m = new MenuItem("57615", "Home");
+            MenuItem m1 = new MenuItem("57721", "Category");
+          
+            Menu.Items.Add(m); Menu.Items.Add(m1);
+        }
+
+        private void ListViewItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MenuItem item = Menu.SelectedItem as MenuItem;
+            switch (item.Title)
+            {
+                case "home":
+                    MainFrame.Navigate(typeof(Pages.Home));
+                    break;
+
+           
+           
+            }
         }
     }
 }
